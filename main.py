3b00185b0ceb1d9f8440ee032705781894b1dfed
Turb0Encabulator <84929895+TurboEncabulat0r@users.noreply.github.com>
2022-04-12 09:12:04 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,44 @@
+import keyboard as kb
+import mouse
+import time 
+
+
+def openChat():
+  kb.press('t')
+  time.sleep(0.05)
+  kb.release('t')
+  time.sleep(0.1)
+
+def type(text, delay=0.04):
+  for i in range(len(text)):
+    kb.press(text[i])
+    time.sleep(delay)
+    kb.release(text[i])
+
+def pressEnter():
+  kb.press('enter')
+  time.sleep(0.3)
+  kb.release('enter')
+  
+
+def goToIs:
+  openChat()
+  type('/is')
+  pressEnter()
+
+def goToHub():
+  openChat()
+  type('/hub')
+  pressEnter()
+ 
+def walkToFarm():
+  kb.press('a')
+  time.sleep(1.3)
+  kb.release('a')
+  
+def startFarming():
+  import macro
+  macro.
+
+
+if __name__ == '__main__':

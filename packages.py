008c995ebdee0b8746130d1fcc90e7e16c48f38f
Turb0Encabulator <84929895+TurboEncabulat0r@users.noreply.github.com>
2022-04-13 09:21:26 -0500

    Added packages to the list, Fixed issue with installer

--- packages.py
+++ packages.py
@@ -7,7 +7,9 @@ packages = [
     'numpy',
     'pillow',
     'pynput',
-    'mouse'
+    'mouse',
+    'discord',
+    'discord.ext'
 ]
 
 
@@ -18,8 +20,11 @@ def install():
             print('installing packages')
             for i in packages:
                 os.system(f'python -m pip install {i}')
+            os.system('cls')
+            print('a restart of the macro is required to finish this')
+            input('press enter to exit')
         elif inp == "N" or inp == 'n':
-            print("a restart of the macro is required")
+            print("the packages are required to run the macro, if you wish to install them manually a list of them are in 'packages.py'")
             input('press enter to exit')
             sys.exit(0)
         else:

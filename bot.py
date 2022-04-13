7ec686ecf4c7439e6167fee1b53a536352eab6d3
Turb0Encabulator <84929895+TurboEncabulat0r@users.noreply.github.com>
2022-04-13 13:46:14 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,23 @@
-import discord
-from discord.ext import commands
+import io, time, random
 import subcommands as sub
-import random, pyautogui, io
 from threading import Thread
-import time
+import packages as pac
+
+def packages():
+    global discord, commands
+    import discord
+    from discord.ext import commands
+    import keyboard, pyautogui, mouse
+    from pynput import keyboard as kb
+    import numpy as np
+    from PIL import Image
+
 bot = commands.Bot(command_prefix='.')
 
 token = 'OTYyODY0OTE5MzQxMDQ3ODkw.YlNv1Q.MDvppJ1GryCnzzMdOZcN4eSbRzU'
 starttime = 0
+
+thread1 = 0
 
 @bot.event
 async def on_ready():
@@ -34,8 +44,8 @@ async def tois(ctx):
 
 
 @bot.command(name='say', brief='says anything in minecraft chat')
-async def say(ctx):
-    imp = ctx.content
+async def say(ctx, *, content)):
+    imp = ctx.message.content
     if imp.find(" ") != -1:
         cmd = imp[0:imp.find(" ")]
         subcmd = imp[imp.find(" ") + 1: len(imp)]
@@ -47,22 +57,23 @@ async def say(ctx):
 
 @bot.command(name='startFarming', brief='forces to start farming, use farmcycle instead')
 async def startFarming(ctx):
+    global starttime, thread1
     await ctx.send('attempting to start farming')
     starttime = time.time()
-    Thread(target=sub.startFarming).start()
+    thread1 = Thread(target=sub.startFarming).start()
 
 
 @bot.command(name='farmcycle', brief='cycles the macro, resets the view and starts farming')
 async def farmcycle(ctx):
-    global starttime
+    global starttime, thread1
     await ctx.send('attempting a full farming cycle')
     starttime = time.time()
-    Thread(target=sub.fullFarmCycle).start()
+    thread1 = Thread(target=sub.fullFarmCycle).start()
 
 
 @bot.command(name='walkleft', brief='make the character walk left')
 async def walkleft(ctx):
-    imp = ctx.content
+    imp = ctx.message.content
     if imp.find(" ") != -1:
         cmd = imp[0:imp.find(" ")]
         subcmd = imp[imp.find(" ") + 1: len(imp)]
@@ -74,8 +85,8 @@ async def walkleft(ctx):
 
 
 @bot.command(name='walkright', brief='make the character walk right')
-async def walkright(ctx, content):
-    imp = content
+async def walkright(ctx, *, content):
+    imp = ctx.message.content
     if imp.find(" ") != -1:
         cmd = imp[0:imp.find(" ")]
         subcmd = imp[imp.find(" ") + 1: len(imp)]
@@ -87,8 +98,8 @@ async def walkright(ctx, content):
 
 
 @bot.command(name='walkforward',brief='make the character walk forwards')
-async def walkforward(ctx):
-    imp = ctx.content
+async def walkforward(ctx:
+    imp = ctx.message.content
     if imp.find(" ") != -1:
         cmd = imp[0:imp.find(" ")]
         subcmd = imp[imp.find(" ") + 1: len(imp)]
@@ -101,7 +112,7 @@ async def walkforward(ctx):
 
 @bot.command(name='walkback', brief='make the character walk backwards')
 async def walkback(ctx):
-    imp = ctx.content
+    imp = ctx.messgae.content
     if imp.find(" ") != -1:
         cmd = imp[0:imp.find(" ")]
         subcmd = imp[imp.find(" ") + 1: len(imp)]
@@ -120,28 +131,29 @@ async def sc(ctx):
         image_binary.seek(0)
         await ctx.send(file=discord.File(fp=image_binary, filename='image.png'))
 
+                      
 @bot.command(name='runtime', brief='tells you how long the macro has been running for')
 async def runtime(ctx):
     if starttime != 0:
         if time.time() - starttime < 60:
             await ctx.send(f'the macro has been running for {time.time() - starttime} sec')
-        if time.time() - starttime < 3600:
+        elif time.time() - starttime < 3600:
             await ctx.send(f'the macro has been running for {(time.time() - starttime)/60} min')
         else:
             await ctx.send(f'the macro has been running for {((time.time() - starttime)/60)/60} hours')
     else:
         await ctx.send(f'the macro is currently not running')
 
+                      
 @bot.command(name='stop', brief='stops the macro')
 async def stop(ctx):
     sub.stopmacro()
     await ctx.send('macro stopped')
-    print('stopping macro')
 
 
 @bot.command(name='test', brief='dev stuff')
 async def test(ctx):
-    ctx.send(f'gaming, {random.uniform(0.100, 0.400)}ms')
+    await ctx.send(f'gaming, {random.uniform(0.100, 0.400)}ms')
 
 
 """
@@ -159,4 +171,9 @@ async def walk(ctx):
 """
 
 if __name__ == '__main__':
+    try:
+        packages()
+    except:
+        pac.install()
+                      
     bot.run(token)

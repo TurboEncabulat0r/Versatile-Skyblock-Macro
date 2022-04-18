d6c7a7d3dc51d7c688ec516b2be83a3b3d3b9d93
Turb0Encabulator <84929895+TurboEncabulat0r@users.noreply.github.com>
2022-04-18 08:08:44 -0500

    Update sugarcane.py

--- modules/sugarcane.py
+++ modules/sugarcane.py
@@ -1,43 +1,60 @@
 import keyboard as kb
 import time
+import mouse
 from threading import Thread
+
+rows = 30
+
+forwardTime = 30
+backtime = 30
+rightTime = 5
+leftTime = 5
 
 run = True
 killThreads = False
-
+row = 0
 completed = [False, False, False, False]
+pressed = [False, False, False, False]
 
 def cycle():
     global run, killThreads, completed
     try:
+        mouse.press('left')
         while run:
-            if not completed[0]:
-                t1 = Thread(target=pressForSeconds, args=(['w', 'a'], 4, 0)).start()
-            elif not completed[1]:
-                t1 = Thread(target=pressForSeconds, args=(['w'], 2, 1)).start()
-            elif not completed[2]:
-                t1 = Thread(target=pressForSeconds, args=(['s', 'a'], 4, 2)).start()
-            elif not completed[3]:
-                t1 = Thread(target=pressForSeconds, args=(['d'], 3, 3)).start()
+            if not completed[0] and not pressed[0]:
+                t1 = Thread(target=pressForSeconds, args=(['w', 'a'], forwardTime, 0)).start()
+            elif not completed[1] and not pressed[1]:
+                t1 = Thread(target=pressForSeconds, args=(['w'], rightTime, 1)).start()
+            elif not completed[2] and not pressed[2]:
+                t1 = Thread(target=pressForSeconds, args=(['s', 'a'], backTime, 2)).start()
+            elif not completed[3] and not pressed[3]:
+                t1 = Thread(target=pressForSeconds, args=(['d'], rightTime, 3)).start()
 
             if all(completed):
                 for i in range(len(completed)):
                     completed[i] = False
+                    row += 1
 
             if killThreads:
                 run = False
                 break
+                
             print('frame')
     finally:
         print('macro stopped')
 
+        
 def releaseKeys():
     keys = 'wasd'
     for i in keys:
          kb.release(i)
+            
+    mouse.release('left')
 
+            
 def pressForSeconds(key, delay, ind):
-    global completed
+    global completed, pressed
+    pressed[ind] = True
     for i in key:
         kb.press(i)
 
@@ -45,8 +62,14 @@ def pressForSeconds(key, delay, ind):
 
     for i in key:
         kb.press(i)
+        
+    pressed[ind] = False
     completed[ind] = True
 
-t2 = Thread(target=cycle).start()
-time.sleep(3)
-killThreads = True
+def startMacro():
+    t2 = Thread(target=cycle).start()
+
+def stopMacro():
+    killThreads = True
+    run = True
+    releaseKeys()
